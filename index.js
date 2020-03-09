5d0d9f19d84823f40b23092d4cd0ac28ef8aa1de
ganesh shahapurkar <ganesh.shahapurkar.46@gmail.com>
2020-03-09 18:43:14 +0530

    hello

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const controllers = require('./server_config/controllers')
+const controllers = require('./server/server_config/controllers')
 const app = express();
 const cors = require('cors')
+const path = require('path')
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
-
-
+app.use(express.static(path.join(__dirname, 'oxotel/public')))
 
 
 app.post('/login', async (req, res) => {
@@ -120,8 +120,10 @@ app.put("/user/:id", async (req, res) => {
 })
 
 
-app.listen(5000, () => {
-    console.log("server running on port 5000")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("server running on port " + PORT)
 })
 
 
